f45471021331bc6640ff0e08077fcdcd556f16aa
Mohammed Majd <m.majd@marcomarabia.net>
2025-11-18 12:49:28 +0300

    end-project

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name")
+    list_filter = ("date","first_name")
+    ordering = ("first_name", )
+
+
+admin.site.register(Form, FormAdmin)
--- job_application/views.py
+++ job_application/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-
+from .forms import ApplicationForm
+from .models import Form
+from django.core.mail import EmailMessage
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            Form.objects.create(first_name=first_name, last_name=last_name,email=email,date=date,occupation=occupation)
+            message = f"Application created successfully"
+            email_message = EmailMessage("Application created successfully", message, "", to=[email])
+            email_message.send()
     return render(request, 'index.html')
